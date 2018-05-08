585026b5eb813a6b9f777226882e0f5fbc94c868
Bruce <sanchez.uragan@gmail.com>
2018-05-08 13:02:01 +0300

    rewrite tests

--- server/tests/test_blog.py
+++ server/tests/test_blog.py
@@ -7,7 +7,7 @@ import time
 
 
 class BlogServerTest(unittest.TestCase):
-    address, port = "127.0.0.1", 8889
+    address, port = "127.0.0.1", 8888
     server = BlogServer(address, port)
 
     @classmethod
@@ -16,54 +16,63 @@ class BlogServerTest(unittest.TestCase):
         cls.cur = sqlite3.connect("blog.db").cursor()
         cls.server_thread.start()
         time.sleep(5)
-    # delete numbers
-    def test1_main_page(self):
-        good_answer = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Title</title>\n    <link rel="stylesheet" href="/css/styles.css">\n</head>\n<body>\n<div class="container">\n    <h1>Hello to the main page of the blog!</h1>\n    <div>\n        <div>\n            <a href="create-post/">Create POST</a>\n        </div>\n        <div>\n            <a href="1/">Read all Posts</a>\n        </div>\n    </div>\n    <img src="/images/cat.jpg">\n</div>\n</body>\n</html>'
+
+    def test_main_page(self):
+        good_answer = 200
         r = requests.get(
             "http://{}:{}/".format(self.address, self.port),
             headers={"Accept": "application/json"}
         )
-        self.assertEqual(good_answer, r.json())
+        self.assertEqual(good_answer, r.status_code)
 
-    def test2_create_page(self):
-        good_answer = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Title</title>\n    <link rel="stylesheet" href="/css/styles.css">\n</head>\n<body>\n<div class="container">\n    <div class="form-wrapper">\n        <form method="POST">\n            <div><label for="title">Title:</label><input id="title" name="title" type="text"></div>\n            <div>\n                <button>create_post</button>\n            </div>\n            <div>\n                <label for="post">POST:</label><textarea name="post" id="post" cols="30" rows="10"></textarea>\n            </div>\n        </form>\n    </div>\n    <a href="/">THis is link to the INDex</a>\n</div>\n</body>\n</html>'
+    def test_create_page(self):
+        good_answer = 200
         r = requests.get(
             "http://{}:{}/create-post".format(self.address, self.port),
             headers={"Accept": "application/json"}
         )
-        self.assertEqual(good_answer, r.json())
+        self.assertEqual(good_answer, r.status_code)
 
-    def test3_blog_page_without_content(self):
-        good_answer = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Title</title>\n    <link rel="stylesheet" href="/css/styles.css">\n</head>\n<body>\n    <div class="container">\n        <div class="header">\n            <h1>It is Awesome BLOG!</h1>\n        </div>\n        <div class="create-post">\n            <a href="/create-post/">create post</a>\n        </div>\n        <div class="title">\n            <p>"None"</p>\n        </div>\n        <div class="navigation">\n            <div class="button-next">\n                <a href="/0/">next</a>\n            </div>\n            <div class="button-index"><a href="/">go to the main page</a></div>\n            <div class="button-previous">\n                <a href="/2/">previous</a>\n            </div>\n        </div>\n    </div>\n</body>\n</html>'
+    def test_blog_page_without_content(self):
+        good_answer = 200
         r = requests.get(
             "http://{}:{}/1".format(self.address, self.port),
             headers={"Accept": "application/json"}
         )
-        self.assertEqual(good_answer, r.json())
+        self.assertEqual(good_answer, r.status_code)
 
-    def test4_create_post(self):
+    def test_create_post(self):
         title, post = "THIS IS TITLE", "THIS IS POST"
+        good_answer = 201
+        r = requests.post(
+            "http://{}:{}/create-post".format(self.address, self.port),
+            data={"title": title, "post": post})
+        _, title_db, post_db = self.cur.execute('select * from blog_table where title="{title}"'.format(
+            title=title
+        )).fetchall()[0]
+        self.assertEqual(title_db, title)
+        self.assertEqual(post_db, post)
+        self.assertEqual(good_answer, r.status_code)
+
+    def test_blog_page_with_created_post(self):
+        good_answer = 200
+        title, post = "CAT", "DOG"
         requests.post(
             "http://{}:{}/create-post".format(self.address, self.port),
             data={"title": title, "post": post})
-        _, title_db, post_db = self.cur.execute("select * from blog_table").fetchall()[0]
-        self.assertEqual(title_db, title)
-        self.assertEqual(post_db, post)
-
-    def test5_blog_page_with_created_post(self):
-        good_answer = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Title</title>\n    <link rel="stylesheet" href="/css/styles.css">\n</head>\n<body>\n    <div class="container">\n        <div class="header">\n            <h1>It is Awesome BLOG!</h1>\n        </div>\n        <div class="create-post">\n            <a href="/create-post/">create post</a>\n        </div>\n        <div class="title">\n            <p>"THIS IS POST"</p>\n        </div>\n        <div class="navigation">\n            <div class="button-next">\n                <a href="/0/">next</a>\n            </div>\n            <div class="button-index"><a href="/">go to the main page</a></div>\n            <div class="button-previous">\n                <a href="/2/">previous</a>\n            </div>\n        </div>\n    </div>\n</body>\n</html>'
         r = requests.get(
             "http://{}:{}/1".format(self.address, self.port),
             headers={"Accept": "application/json"}
         )
-        self.assertEqual(good_answer, r.json())
+        self.assertEqual(post, r.json()['html'])
+        self.assertEqual(good_answer, r.status_code)
 
-    def test6_bad_page(self):
-        good_answer = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>404</title>\n</head>\n<body>\n\n    <h1 align="center">404</h1>\n\n</body>\n</html>'
+    def test_bad_page(self):
+        good_answer = 404
         r = requests.get(
             "http://{}:{}/12wdfssdfw234".format(self.address, self.port),
             headers={"Accept": "application/json"})
-        self.assertEqual(good_answer, r.json())
+        self.assertEqual(good_answer, r.status_code)
 
 
 if __name__ == "__main__":
